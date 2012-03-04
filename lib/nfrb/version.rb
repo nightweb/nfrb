636d36fb878f058fa030bb50b78c6914da17106d
Davide Guerri <davide.guerri@gmail.com>
2012-03-04 18:24:51 +0100

    bump version

--- lib/nfrb/version.rb
+++ lib/nfrb/version.rb
@@ -1,6 +1,6 @@
 # This file is part of the nfrb gem for Ruby.
 #
-# Copyright (C) 2011 Davide Guerri
+# Copyright (C) 2012 Davide Guerri
 # 
 # This code is largely derived from nfreader.c of nfdump suite.
 
@@ -8,8 +8,8 @@ module NfRb
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 1
-    BUILD = 'release'
+    PATCH = 2
+    BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
